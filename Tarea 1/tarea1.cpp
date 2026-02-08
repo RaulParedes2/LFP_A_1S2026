10843a4b081eb63862232203f4131297a1001a30
Raul Paredes <3015806690101@ingenieria.usac.edu.gt>
2026-02-08 13:13:16 -0600

    Tarea 1

---
+++ Tarea 1/tarea1.cpp
@@ -0,0 +1,56 @@
+#include<iostream>;
+
+using namespace std;
+
+class gato{
+
+    private:
+        string nombre;
+        int edad;
+        string color;
+
+    public:
+    gato(string n, int e, string c){
+        nombre = n ;
+        edad = e;
+        color = c;
+
+    }
+
+    void maullar(){
+        cout<<nombre<< ": Dice Miauuuuuuu"<<endl;
+    }
+
+    void comer(){
+        cout<<nombre<<": Esta comiendo..."<<endl;
+    }
+
+    void  mostrarInformacion(){
+        cout<<"Nombre: "<< nombre <<endl;
+        cout<<"Edad: "<< edad <<endl;
+        cout<<"Color: "<< color <<endl;
+    }
+
+};
+
+int main(){
+
+    gato gato1("Pichi", 5, "Negro");
+    gato gato2("Mandarino", 3, "Naranjoso");
+
+    cout<<"Gato 1: " << endl;
+    gato1.mostrarInformacion();
+    gato1.maullar();
+    gato1.comer();
+
+    cout << endl;
+
+    cout<<"Gato 2: " <<endl;
+    gato2.mostrarInformacion();
+    gato2.maullar();
+    gato2.comer();
+
+
+
+    return 0;
+}
