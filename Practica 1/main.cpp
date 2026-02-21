907d5d9052468cc4e2b4e3ec41072781b446cd98
Raul Paredes <3015806690101@ingenieria.usac.edu.gt>
2026-02-20 21:51:57 -0600

    algunos manejos de error

---
+++ Practica 1/main.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <exception>
+#include "sistema.h"
+
+using namespace std;
+
+int main(){
+
+    Sistema sistema;
+    int opcion;
+    string ruta;
+
+    do{
+
+        cout <<"\n======================================\n";
+        cout <<"SISTEMA DE ANALISIS ACADEMICO \n";
+        cout <<"======================================\n";
+        cout <<"1. Cargar archivo del estudiante\n";
+        cout <<"2. Cargar archivo de cursos\n";
+        cout <<"3. cargar archivo de notas\n";
+        cout <<"4. Generar Reporte: Estadistica por curso\n";
+        cout <<"5. Generar Reporte: Redimiento por estudiante\n";
+        cout <<"6. Generar Reporte: Top 10 Mejores Estudiantes\n";
+        cout <<"7. Generar Reporte: Cursos con Mayor aprobacion\n";
+        cout <<"8. Generar Reporte: Analisis por Carrera\n";
+        cout <<"9. Salir\n";
+        cout <<"Seleccione una opcion: ";
+        cin >> opcion;
+
+        if(cin.fail()){
+            cin.clear();
+            cin.ignore(1000, '\n');
+            cout << "Entrada invalida.\n";
+            continue;
+
+        }
+
+        try{
+        switch (opcion)
+        {
+        case 1 :
+            cout<<"Ingrese ruta del archivo estudiantes.lfp: ";
+            cin.ignore();
+            getline(cin, ruta); 
+            sistema.cargarEstudiantes(ruta);
+            break;
+        
+            case 2 :
+            cout<<"Ingrese ruta del archivo cursos.lfp: ";
+            cin.ignore();
+            getline(cin, ruta); 
+            sistema.cargarCursos(ruta);
+            break;
+
+            case 3 :
+            cout<<"Ingrese ruta del archivo notas.lfp: ";
+            cin.ignore();
+            getline(cin, ruta); 
+            sistema.cargarNotas(ruta);
+            break;
+
+            case 9:
+            cout<<"Saliendo del sistema...\n";
+            break;
+
+            default:
+            cout<<"Opcion invalida.\n";
+            break;
+        }
+    }catch(const exception& e){
+        cout<<"Error: "<< e.what() << endl;
+    }
+
+    }
+    while(opcion != 9);
+
+    return 0 ;
+}
--- Practica 1/sistema.cpp
+++ Practica 1/sistema.cpp
@@ -2,34 +2,185 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <stdexcept>
+#include <exception>
 
 using namespace std;
 
-void Sistema:: cargarEstudiantes(string ruta){
+//----------------------------------------------------------------------------
+// Carga de Estudiantes
+//----------------------------------------------------------------------------
 
-    ifstream archivo (ruta);
+void Sistema::cargarEstudiantes(string ruta)
+{
+
+    ifstream archivo(ruta);
+    try
+    {
+        if (!archivo.is_open())
+        {
+            throw runtime_error("Error al cargar el archivo: " + ruta);
+            return;
+        }
+    }
+    catch (const exception &e)
+    {
+        cout << "Error: " << e.what() << endl;
+    }
     string linea;
 
-    while(getline(archivo, linea)){
+    while (getline(archivo, linea))
+    {
+
+        if (linea.empty())
+            continue;
+
         stringstream ss(linea);
         string dato;
-        vector<string>partes;
+        vector<string> partes;
 
-        while(getline(ss, dato,'s')){
+        while (getline(ss, dato, ','))
+        {
             partes.push_back(dato);
         }
-        if(partes.size() == 5){
-            Estudiante e(
-                stoi(partes[0]),
-                partes[1],
-                partes[2],
-                partes[3],
-                stoi(partes[4])
-            );
-            estudiantes.push_back(e);
+        if (partes.size() == 5)
+        {
+            try
+            {
+                Estudiante e(
+                    stoi(partes[0]),  // entero
+                    partes[1],        // string
+                    partes[2],        // string
+                    partes[3],        // string
+                    stoi(partes[4])); // entero
+                estudiantes.push_back(e);
+            }
+            catch (...)
+            {
+                cout << "Error de Datos de Estudiante.\n";
+            }
         }
     }
 
     archivo.close();
-    cout << "Estudiante cargados correctamente. \n";
+    cout << "Archivo Cerrado \n";
+}
+//----------------------------------------------------------------------------
+// Carga de cursos
+//----------------------------------------------------------------------------
+
+void Sistema::cargarCursos(string ruta)
+{
+    ifstream archivo(ruta);
+
+    try
+    {
+        if (!archivo.is_open())
+        {
+            throw runtime_error("Error al cargar el archivo Cursos" + ruta);
+            return;
+        }
+    }
+    catch (const exception &e)
+    {
+        cout << "Error: " << e.what() << endl;
+    }
+
+    string linea;
+
+    while (getline(archivo, linea))
+    {
+        if (linea.empty())
+            continue;
+
+        stringstream ss(linea);
+        string dato;
+        vector<string> partes;
+        while (getline(ss, dato, ','))
+        {
+            partes.push_back(dato);
+        }
+
+        if (partes.size() == 5)
+        {
+            try
+            {
+                Curso c(
+                    stoi(partes[0]), // entero
+                    partes[1],       // string
+                    stoi(partes[2]), // entero
+                    stoi(partes[3]), // entero
+                    partes[4]        // string
+                );
+                cursos.push_back(c);
+            }
+            catch (...)
+            {
+                cout << "Error en los datos de cursos. \n";
+            }
+        }
+    }
+    archivo.close();
+    cout << "Archivo Cerrado.\n";
+}
+//----------------------------------------------------------------------------
+// Carga de Notas
+//----------------------------------------------------------------------------
+
+void Sistema::cargarNotas(string ruta)
+{
+
+    ifstream archivo(ruta);
+
+    try
+    {
+        if (!archivo.is_open())
+        {
+            throw runtime_error("Error no se pudo abrir el archivo: " + ruta);
+            return;
+        }
+    }
+    catch (const exception &e)
+    {
+        cout << "Error" << e.what() << endl;
+    }
+
+    string linea;
+
+    while (getline(archivo, linea))
+    {
+        if (linea.empty())
+            continue;
+
+        stringstream ss(linea);
+        string dato;
+        vector<string> partes;
+
+        while (getline(ss, dato, ','))
+        {
+            partes.push_back(dato);
+        }
+
+        if (partes.size() == 5)
+        {
+
+            try
+            {
+                Nota n(
+                    stoi(partes[0]),  // entero
+                    stoi(partes[1]),  // entero
+                    stod(partes[2]),  // double
+                    partes[3],        // string
+                    stoi(partes[4])); // entero
+
+                notas.push_back(n);
+            }
+            catch (...)
+            {
+                cout << "Error en los datos.\n";
+            }
+        }
+    }
+    archivo.close();
+    cout << " Archivo cerardo.\n";
 }
